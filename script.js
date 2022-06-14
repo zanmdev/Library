ec58edbff9fe28f98c6f49311b6e38c92cff88ea
Sunny <sunnyb67890@hotmail.com>
2022-06-14 01:49:39 -0400

    Completed displayBook function

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let bookLibrary = [];
 
 
-function Book(name,author,year, totalPages, pagesRead,hasBeenRead){
+function Book(name,author,year, totalPages,hasBeenRead){
     this.name = name;
     this.author = author;
     this.year = year;
     this.hasBeenRead = hasBeenRead;
     this.totalPages = totalPages;
-    this.pagesRead = pagesRead;
+    
 }
 
-function addBookToLibrary(name,author,year,totalPages, pagesRead ,hasBeenRead){
+function addBookToLibrary(name,author,year,totalPages ,hasBeenRead){
 
-    bookLibrary.push(new Book(name,author,year,totalPages, pagesRead ,hasBeenRead));
+    bookLibrary.push(new Book(name,author,year,totalPages ,hasBeenRead));
     showBooksArray();
 }
 
@@ -37,16 +37,42 @@ function showBooksArray(){
 
         
         card.classList.add("book");
-        bookTitle.classList.add("book-title");
-        author.classList.add("author");
-        year.classList.add("year");
         card.id  = index;
-       
 
-        text.innerText = book.name
+        bookTitle.textContent = book.name;
+        author.textContent = book.author;
+        year.textContent = book.year;
+        pages.textContent = book.pages;
 
 
-        card.appendChild(text);
+        card.appendChild(bookTitle);
+        card.appendChild(author);
+        card.appendChild(year);
+        card.appendChild(pages);
+
+        console.log(book.hasBeenRead);
+        if(book.hasBeenRead){
+            card.classList.add("read");
+            readButton.textContent = "Finished";
+            readButton.classList.add("finish-btn");
+        }else{
+            card.classList.add("not-read");
+            readButton.textContent = "Not Finished";
+            readButton.classList.add("unfinish-btn");
+        }
+        removeButton.textContent = "Remove";
+        buttonContainer.classList.add("buttons");
+        buttonContainer.appendChild(readButton);
+        buttonContainer.appendChild(removeButton);
+
+
+        card.appendChild(buttonContainer);
+
+
+        
+
+
+        
 
         
         container.appendChild(card);
